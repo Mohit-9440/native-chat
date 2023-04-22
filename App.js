bf433202a6f8d9468004b5e3db910f4b4ae2819f
Mohit <mohitsharma8864@gmail.com>
2023-04-23 01:33:23 +0530

    sign up screen

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import Chat from "./screens/Chat";
 import Login from "./screens/Login";
+import Signup from "./screens/Signup";
 
 const Stack = createStackNavigator();
 
 function ChatStack () {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Chat" component={Login}/>
+    <Stack.Navigator screenOptions={{headerShown : false}}>
+      <Stack.Screen name="Signup" component={Signup} />
     </Stack.Navigator>
   )
 }
