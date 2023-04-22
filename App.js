c568446454b98bd75ac138f10f9a8f3e62946da0
Mohit <mohitsharma8864@gmail.com>
2023-04-23 01:38:30 +0530

    home screen

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Chat from "./screens/Chat";
 import Login from "./screens/Login";
 import Signup from "./screens/Signup";
+import Home from "./screens/Home";
 
 const Stack = createStackNavigator();
 
