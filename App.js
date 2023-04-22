59dcdc4f2a17db45a0a642cfefc78605f75d723f
Mohit <mohitsharma8864@gmail.com>
2023-04-23 02:03:55 +0530

    authentication

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React,{useState, createContext, useContext, useEffect} from "react";
+import { View, ActivityIndicator } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from './config/firebase';
 
 import Chat from "./screens/Chat";
 import Login from "./screens/Login";
@@ -8,22 +11,69 @@ import Signup from "./screens/Signup";
 import Home from "./screens/Home";
 
 const Stack = createStackNavigator();
+const AuthenticatedUserContext = createContext({});
+
+const AuthenticatedUserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  return (
+    <AuthenticatedUserContext.Provider value={{user, setUser}}>
+      {children}
+    </AuthenticatedUserContext.Provider>
+  )
+}
 
 function ChatStack () {
   return (
-    <Stack.Navigator screenOptions={{headerShown : false}}>
+    <Stack.Navigator defaultScreenOptions={Home}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Chat" component={Chat} />
+    </Stack.Navigator>
+  )
+}
+
+function AuthStack() {
+  return(
+    <Stack.Navigator defaultScreenOptions={Login} screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Signup" component={Signup} />
     </Stack.Navigator>
   )
 }
 
 function RootNavigator () {
+  const { user, setUser } = useContext(AuthenticatedUserContext);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    // onAuthStateChanged returns an unsubscriber
+    const unsubscribeAuth = onAuthStateChanged(
+      auth,
+      async authenticatedUser => {
+        authenticatedUser ? setUser(authenticatedUser) : setUser(null);
+        setIsLoading(false);
+      }
+    );
+// unsubscribe auth listener on unmount
+    return unsubscribeAuth;
+  }, [user]);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size='large' />
+      </View>
+    );
+  }
+
   return (
-    <NavigationContainer>
-      <ChatStack />
-    </NavigationContainer>
-  )
-}
+      <NavigationContainer>
+        {user ? <ChatStack /> : <AuthStack />}
+      </NavigationContainer>
+    );
+  }
 export default function App() {
-  return <RootNavigator/>
+  return (
+    <AuthenticatedUserProvider>
+      <RootNavigator/>
+    </AuthenticatedUserProvider>
+  )
 }
